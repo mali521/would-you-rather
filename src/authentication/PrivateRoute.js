656c38e3f9c12f0a859ba323977f01f762267416
Mudassir Gadit <mud27747@gmail.com>
2020-04-25 17:57:09 +0500

    first commit

---
+++ src/authentication/PrivateRoute.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { fakeAuth } from './auth'
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={(props) => (
+        fakeAuth.isAuthenticated === true 
+        ? <Component {...props} />
+        : <Redirect to={{
+            pathname: '/login',
+            state: {from: props.location}
+        }} />
+    )} />
+)
+
+export default PrivateRoute
